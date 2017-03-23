331c8cfff03994cf9bd54bb1c2953a7eb8b45a81
Matthew Snook <mrsnook42@gmail.com>
2017-03-23 17:59:09 +0000

    Added classes to bankr.css and added spending viewChanges made by Matthew Snook (mrsnook42@gmail.com).

--- Bankr/components/spendingView/index.js
+++ Bankr/components/spendingView/index.js
@@ -1,12 +1,33 @@
 'use strict';
 
 app.spendingView = kendo.observable({
-    onShow: function() {},
-    afterShow: function() {}
+    onShow: function () { },
+    afterShow: function () { }
 });
 app.localization.registerView('spendingView');
 
 // START_CUSTOM_CODE_spendingView
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 
+app.spendingView = kendo.observable({
+    onShow: function () {
+        var data = [];
+
+        expensesDataSource._data.forEach(function(expense){
+            var obj = {};
+            obj.Id = expense.Id;
+            obj.CreatedAt = expense.CreatedAt;
+            obj.ExpenseName = expense.ExpenseName;
+            obj.ExpenseCategory = expense.ExpenseCategory.CategoryName;
+            obj.ExpenseAmount = Math.round(parseFloat(expense.ExpenseAmount)).toFixed(2);
+            data.push(obj);
+        });
+
+        $("#spending-list").kendoMobileListView({
+            dataSource: data,
+            template: '<div><strong>#: ExpenseName # - #: ExpenseCategory #</strong></div> <div>#: CreatedAt.toDateString() # <span class="spending-list-right"><a data-role="button" href="components/editExpenseView/view.html?id=#: Id #">Edit</a></span></div><div>#: ExpenseAmount # $</div>'
+        });
+    },
+    afterShow: function () { }
+});
 // END_CUSTOM_CODE_spendingView
