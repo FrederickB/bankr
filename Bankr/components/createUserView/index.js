fa73a0d235500afea371de2e63d01c0d9bd7fdb4
Julien Clément <2645417@edu.collegelacite.ca>
2017-03-30 16:57:32 +0000

    Added a Go Back button on the forgotPasswordView and the createUserView and fixed navigation issues on both views. Changes made by Julien Clément (2645417@edu.collegelacite.ca).

--- Bankr/components/createUserView/index.js
+++ Bankr/components/createUserView/index.js
@@ -9,27 +9,34 @@ app.localization.registerView('createUserView');
 // START_CUSTOM_CODE_createUserView
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 
-app.createUserView = kendo.observable({
-    submit: function() {
-        if (!this.username) {
-            navigator.notification.alert("Username is required.");
-            return;
-        }
-        if (!this.password) {
-            navigator.notification.alert("Password is required.");
-            return;
-        }
-        app.data.backendServices.Users.register(this.username, this.password, {
+(function (parent) {
+    var createUserViewModel = kendo.observable({
+        submit: function () {
+            if (!this.username) {
+                navigator.notification.alert("Username is required.");
+                return;
+            }
+            if (!this.password) {
+                navigator.notification.alert("Password is required.");
+                return;
+            }
+            app.data.backendServices.Users.register(this.username, this.password, {
                 Email: this.email
             },
-            function() {
-                navigator.notification.alert("Your account was successfully created.");
-                app.mobileApp.navigate("components/loginView/view.html");
-            },
-            function() {
-                navigator.notification.alert("Unfortunately we were unable to create your account.");
-            });
-    }
-});
+                function () {
+                    navigator.notification.alert("Your account was successfully created.");
+                    app.mobileApp.navigate("components/loginView/view.html");
+                },
+                function (err) {
+                    navigator.notification.alert("Unfortunately we were unable to create your account.");
+                });
+        },
+        goBack: function() {
+            app.mobileApp.navigate('components/loginView/view.html');
+        }
+    });
+
+    parent.createUserViewModel = createUserViewModel;
+})(app.createUserView);
 
 // END_CUSTOM_CODE_createUserView
