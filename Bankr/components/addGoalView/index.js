98784f5635d04e85186bbcd2f51588eb810fff52
Cyrille Mambou <cyrillemambou17@gmail.com>
2017-04-06 18:55:23 +0000

    Add goalView, addGoalView, editGoalView Changes made by Cyrille Mambou (cyrillemambou17@gmail.com).

--- Bankr/components/addGoalView/index.js
+++ Bankr/components/addGoalView/index.js
@@ -1,12 +1,88 @@
 'use strict';
-
 app.addGoalView = kendo.observable({
     onShow: function() {},
     afterShow: function() {}
 });
+
 app.localization.registerView('addGoalView');
 
 // START_CUSTOM_CODE_addGoalView
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 
+(function(parent){
+    var addGoalViewModel = kendo.observable({
+        goBack: function() {
+            app.mobileApp.navigate('components/goalsView/view.html');
+        },
+        submit: function(event) {
+            event.preventDefault();
+
+            var title = this.goalTitle;
+            var categoryId = this.selects;
+            var amount = this.goalAmount;
+            var dateTime = $('#goalDateTimeStart').val();
+            var dateTimeEnd = $('#goalDateTimeEnd').val();
+
+             if(!title){
+                navigator.notification.alert('title is required');
+                return;
+            } 
+            else if(!amount){
+                navigator.notification.alert('Amount is required');
+                return;
+            } 
+            else if(!categoryId){
+                navigator.notification.alert('Category is required');
+                return;
+            }else if(!dateTime) {
+                navigator.notification.alert('Date is required');
+                return;
+            }else if(!dateTimeEnd) {
+                navigator.notification.alert('Date is required');
+                return;
+            }
+            amount = parseFloat(parseFloat(amount).toFixed(2));
+
+            var goal = {
+                GoalCategory: categoryId,
+                GoalStartDate: dateTime,
+                GoalEndDate: dateTimeEnd,
+                GoalAmount: amount,
+                GoalTitle: title
+            };
+
+            goalsDataSource.add(goal);
+            goalsDataSource.one('sync', addGoalViewModel.readAndClose);
+            goalsDataSource.sync();
+        },
+        readAndClose: function () {
+            goalsDataSource.read().then(function(){
+                app.mobileApp.navigate('components/goalsView/view.html');
+            });
+        }
+    });
+
+    parent.onShow = function() {
+        $('#goalAmount').val('');
+        $('#goalTitle').val('');
+        $('#addGoalCategories').val($('#selects option:first').val());
+        $('#goalDateTimeStart').val('');
+        $('#goalDateTimeEnd').val('');
+    };
+
+    parent.addGoalViewModel = addGoalViewModel;
+})(app.addGoalView);
+
+function initAddGoalView(){
+    $('#goalDateTimeStart').kendoDateTimePicker({
+        value: new Date()
+    });
+    $('#goalDateTimeEnd').kendoDateTimePicker({
+        value: new Date()
+     });
+
+    categoriesDataSource._data.forEach(function(category){
+        $('#addGoalCategories').append('<option value="' + category.Id + '">' + category.CategoryName + '</option>');
+    });
+}
 // END_CUSTOM_CODE_addGoalView
--- Bankr/components/goalsView/index.js
+++ Bankr/components/goalsView/index.js
@@ -1,12 +1,51 @@
-'use strict';
-
 app.goalsView = kendo.observable({
-    onShow: function() {},
-    afterShow: function() {}
+    onShow: function () { },
+    afterShow: function () { }
 });
 app.localization.registerView('goalsView');
 
 // START_CUSTOM_CODE_goalsView
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 
+(function (parent) {
+    var viewModel = kendo.observable({
+        editGoal: function (event) {
+            var id = event.target[0].getAttribute('itemid');
+            app.editGoalView.set('id', id);
+            app.mobileApp.navigate('components/editGoalView/view.html');
+        }
+    });
+
+    parent.onShow = function () {
+        var data = [];
+        goalsDataSource._data.forEach(function (goal) {
+            var obj = {};
+            obj.GoalAmount = goal.GoalAmount;
+            obj.GoalCategory = goal.GoalCategory.CategoryName;
+            var sum = 0;
+            expensesDataSource._data
+                .filter(function (expense) {
+                    return expense.ExpenseCategory.Id === goal.GoalCategory.Id;
+                })
+                .forEach(function (expense) {
+                    sum += expense.ExpenseAmount;
+                });
+
+            obj.ExpenseAmount = sum;
+            obj.GoalTitle = goal.GoalTitle;
+            obj.GoalStartDate = goal.GoalStartDate;
+            obj.GoalEndDate = goal.GoalEndDate;
+            obj.Id = goal.Id;
+            data.push(obj);
+        })
+
+
+        $("#goals-list").kendoMobileListView({
+            dataSource: data,
+            template: '<div><strong>#: GoalTitle # - #: GoalCategory #</strong></div><div><span style="float:right"><a data-role="button" data-bind="click: editGoal" itemid="#: Id #">Edit</a></span></div><div></div><div><strong>#: (parseFloat(Math.round(ExpenseAmount * 100) /100).toFixed(2)) #$ / #: (parseFloat(Math.round(GoalAmount * 100) /100).toFixed(2)) #$</strong></div><div>From #: GoalStartDate # to #: GoalEndDate #</div>'
+        });
+
+        kendo.bind($('#goals-list li a'), viewModel);
+    }
+})(app.goalsView);
 // END_CUSTOM_CODE_goalsView
