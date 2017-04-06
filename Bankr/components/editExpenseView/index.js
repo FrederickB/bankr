518d28366468a98db6d76d671b8b45560cd8e8d0
Leon Ohoue <lleon2255@gmail.com>
2017-04-06 18:53:14 +0000

    Cleaned up code of editExpenseView. Changes made by Leon Ohoue (lleon2255@gmail.com).

--- Bankr/components/editExpenseView/index.js
+++ Bankr/components/editExpenseView/index.js
@@ -13,11 +13,12 @@ app.localization.registerView('editExpenseView');
         var id = parent.id;
         var currentExpense = expensesDataSource.get(id);
 
-        $('#Name').val(currentExpense.ExpenseName);
-        $('#Date').data('kendoDateTimePicker').value(new Date(currentExpense.ExpenseDateTime));
-        $('#Amount').val(parseFloat(currentExpense.ExpenseAmount).toFixed(2));
-        $('#Select option[value="' + currentExpense.ExpenseCategory.Id + '"]').attr('selected', 'selected');
+        $('#editExpenseName').val(currentExpense.ExpenseName);
+        $('#editExpenseDate').data('kendoDateTimePicker').value(new Date(currentExpense.ExpenseDateTime));
+        $('#editExpenseAmount').val(parseFloat(currentExpense.ExpenseAmount).toFixed(2));
+        $('#editExpenseCat option[value="' + currentExpense.ExpenseCategory.Id + '"]').attr('selected', 'selected');
     };
+
     var editExpenseViewModel = kendo.observable({
         goBack: function () {
             app.mobileApp.navigate('components/spendingView/view.html');
@@ -25,29 +26,28 @@ app.localization.registerView('editExpenseView');
 
         submit: function (event) {
             event.preventDefault();
-            console.log('stil working');
 
             var id = parent.id;
             var currentExpense = expensesDataSource.get(id);
 
-            if ($('#Name').val().trim() === '') {
+            if ($('#editExpenseName').val().trim() === '') {
                 navigator.notification.alert("Name is required");
                 return;
-            } else if ($('#Amount').val() === '') {
+            } else if ($('#editExpenseAmount').val() === '') {
                 navigator.notification.alert("Amount is required");
                 return;
             }
             this.Amount = parseFloat(parseFloat(this.Amount).toFixed(2));
-            currentExpense.set('ExpenseName', this.Name);
-            currentExpense.set('ExpenseAmount', this.Amount);
-            currentExpense.set('ExpenseCategory', this.Category);
-            currentExpense.set('ExpenseDateTime', this.Date);
-            console.log('done');
+            currentExpense.set('ExpenseName', this.editExpenseName);
+            currentExpense.set('ExpenseAmount', this.editExpenseAmount);
+            currentExpense.set('ExpenseCategory', this.editExpenseCat);
+            currentExpense.set('ExpenseDateTime', this.editExpenseDate);
+
             expensesDataSource.one('sync', editExpenseViewModel.readAndClose);
             expensesDataSource.sync();
         },
         readAndClose: function () {
-            expensesDataSource.read().then(function(){
+            expensesDataSource.read().then(function () {
                 app.mobileApp.navigate('components/spendingView/view.html');
             });
         }
@@ -57,12 +57,12 @@ app.localization.registerView('editExpenseView');
 })(app.editExpenseView);
 
 function initEdiExpenseView() {
-    $('#Date').kendoDateTimePicker({
+    $('#editExpenseDate').kendoDateTimePicker({
         value: new Date()
     });
 
     categoriesDataSource._data.forEach(function (category) {
-        $('#Select').append('<option value="' + category.Id + '">' + category.CategoryName + '</option>');
+        $('#editExpenseCat').append('<option value="' + category.Id + '">' + category.CategoryName + '</option>');
     });
 }
 
